5928dea19ba65bff1925882004c1ee0fbfb7baee
Mauricio Cardoso Oliveira <89526223+MauricioDevJS@users.noreply.github.com>
2023-04-28 19:40:54 -0300

    Atualização aula 28/04/2023

---
+++ carrinho.js
@@ -0,0 +1,8 @@
+var carrinho = {
+    itens: [
+      { nome: "T-shirt Masculina", preco: 49.99, quantidade: 2 },
+      { nome: "T-shirt Feminina", preco: 39.99, quantidade: 1 }
+    ]
+  }
+
+  console.log(carrinho)
